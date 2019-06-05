b6b59c397d419c9f7bea4614d39bb8f7ca98f1b4
John Yu <j.yu13@share.epsb.ca>
2019-06-04 19:50:59 -0600

    Finished endscreen

--- Endscreen.py
+++ Endscreen.py
@@ -1,14 +1,23 @@
 # This file creates the endscreen #
 from Mixer import *
+from TextFile import *
 
 
 class Endscreen:
-    def __init__(self, song):
+    def __init__(self, song, status):
+        color = {
+            "You Win": (0, 255, 0),
+            "You Suck": (255, 0, 0)
+        }
         self.screen_dim = (800, 600)
         self.display = pygame.display.set_mode(self.screen_dim)
         self.song_name = song
         self.music = Music()
         self.music.set_sound(self.song_name)
+        self.status = Text(status, self.display) # Win, lose, or tie
+        self.status.set_size(80)
+        self.status.set_pos((400 - (self.status.get_size()[0])/2, 300 - (self.status.get_size()[1])/2))
+        self.text_back = Background(color[status], (self.status.get_size()[0] + 30, self.status.get_size()[1] + 30), (400 - (self.status.get_size()[0] + 30)/2, 300 - (self.status.get_size()[1] + 30)/2), self.display)
 
     def run_endscreen(self):
         pygame.init()
@@ -20,5 +29,9 @@ class Endscreen:
                     run = False
 
             self.display.fill((255, 255, 255))
+
+            # Draw Sprites #
+            self.text_back.draw()
+            self.status.draw()
 
             pygame.display.update()
--- main.py
+++ main.py
@@ -201,13 +201,13 @@ while run:
         output = network.send_and_receive(
             [local.get_selection().get_id(), (local.get_selection().getx(), local.get_selection().gety()),
              chat_to_send])
-        endscreen = Endscreen('boo')
+        endscreen = Endscreen('boo', "You Suck")
         endscreen.run_endscreen()
     if len(enemy.get_list()) == 0:  # You win
         output = network.send_and_receive(
             [local.get_selection().get_id(), (local.get_selection().getx(), local.get_selection().gety()),
              chat_to_send])
-        endscreen = Endscreen('applause')
+        endscreen = Endscreen('applause', "You Win")
         endscreen.run_endscreen()
 
     posMove = 1
@@ -221,7 +221,7 @@ while run:
                 output = network.send_and_receive(
                     [local.get_selection().get_id(), (local.get_selection().getx(), local.get_selection().gety()),
                      chat_to_send])
-                endscreen = Endscreen('applause')
+                endscreen = Endscreen('applause', "You Win")
                 endscreen.run_endscreen()
                 break
         print('Tie')
